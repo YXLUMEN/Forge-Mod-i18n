9eea99eda0f2d876e65dc35da52685c703cee9e1
IceLens <wenyiming0325@163.com>
2023-07-23 21:30:26 +0800

    添加导入库失败时的错误处理

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+﻿import argparse
+import json
 import os
 import re
 import shutil
+import time
 import zipfile
-import json
-import argparse
-from time import sleep
 from concurrent.futures import ProcessPoolExecutor
 
 parsers = argparse.ArgumentParser()
@@ -24,8 +24,9 @@ while count:
         print(E)
         os.system('pip install json5')
         count -= 1
+if count <= 0:
+    print("Json5 库似乎未成功安装,可能无法处理部分文件")
 del count
-del E
 
 
 class LangTransHelper:
@@ -48,6 +49,11 @@ class LangTransHelper:
 
     # extract lang files from jar(zip)
     def extract_mod_lang(self, zip_file):
+        """
+        Extract lang files from the mod jar (compress file)
+
+        zip_file is a string of compress file's name
+        """
         # semaphore
         try:
             zip_f = zipfile.ZipFile(zip_file, 'r')
@@ -73,7 +79,13 @@ class LangTransHelper:
         return mod_id
 
     def read_resource_pack_lang(self, file: str):
-        mod = None
+        """
+
+        :param file: Resource pack's name
+        :return:
+
+        Check if resource pack is valid,if true copy the lang files
+        """
         # resource pack usually is a zip file
         if zipfile.is_zipfile(file):
             try:
@@ -90,10 +102,12 @@ class LangTransHelper:
             for lang_json in target.namelist():
                 # get mod id in pack
                 # usually it is the name of folder
-                if lang_json.split('/')[0] == 'assets' and len(lang_json.split('/')) == 2:
-                    mod = lang_json.split('/')[-1]
-                self._zip_reader(target, lang_json, mod)
-                target.close()
+                if lang_json.split('/')[0] == 'assets' and len(lang_json.split('/')) > 3:
+                    mod = lang_json.split('/')[1]
+                    if not os.access(f'{self.output_dir}/has_trans/{mod}/', os.W_OK):
+                        os.makedirs(f'{self.output_dir}/has_trans/{mod}/')
+                    self._zip_reader(target, lang_json, mod)
+            target.close()
         # sometime resource pack is a folder
         elif os.path.isdir(file):
             # check pack.mcmeta
@@ -111,8 +125,14 @@ class LangTransHelper:
             print('\033[31m未解析的资源包\033[0m -> ' + file.split('\\')[-1])
             return
 
-    # read_resource_pack_lang's part TO make main function more beautiful
     def _read_resource_pack_part_one(self, each_mod):
+        """
+
+        :param each_mod:
+        :return:
+
+        Read_resource_pack_lang's part. TO make main function more clean
+        """
         # split path to get folder name
         mod_id = each_mod.split('\\')[-1]
         try:
@@ -131,12 +151,12 @@ class LangTransHelper:
     # func 1
     def replace_authority_lang(self, first_list: list, second_list: list):
         # for resource pack
-        for k in second_list:
-            self.read_resource_pack_lang(k)
+        for each_pack in second_list:
+            self.read_resource_pack_lang(each_pack)
         os.rename(f'{self.output_dir}/has_trans', f'{self.output_dir}/temp')
         # for mods
-        for k in first_list:
-            mod_id = self.extract_mod_lang(k)
+        for each_pack in first_list:
+            mod_id = self.extract_mod_lang(each_pack)
             try:
                 # load as json
                 origin = open(f'{self.output_dir}/has_trans/{mod_id}/{self.lang}.json', 'rb')
@@ -146,7 +166,7 @@ class LangTransHelper:
             except FileNotFoundError:
                 continue
             except Exception as e:
-                print(f'func1 出现错误: {e}')
+                print(f'官方翻译替换 \033[31m出现错误:\033[0m {e}')
                 continue
             origin.update(after)
             latest = open(f'{self.output_dir}/has_trans/{mod_id}/{self.lang}.json', 'w', encoding='utf-8')
@@ -198,7 +218,7 @@ class LangTransHelper:
             return
         # use json5
         except json.decoder.JSONDecodeError:
-            print('\033[33mMod的语言文件中包含注释\033[0m -> ' + mod_id.split('/')[-1])
+            print('\033[33m使用json5处理文件\033[0m -> ' + mod_id.split('/')[-1])
             try:
                 en_file = open(f'{mod_id}/en_us.json', 'rb')
                 en_file = json5.load(en_file)
@@ -206,8 +226,14 @@ class LangTransHelper:
                 zh_file = json5.load(zh_file)
             except FileNotFoundError:
                 return
+            except ImportError:
+                print(f'\033[31mJson5 未安装,无法处理\033[0m -> ' + mod_id.split('/')[-1])
+                return
+            except Exception as e:
+                print(f'\033[31mjson5 处理出错: {e}\033[0m -> ' + mod_id.split('/')[-1])
+                return
         except Exception as e:
-            print(f'func2 \033[31m出现错误:\033[0m {e} -> ' + mod_id.split('/')[-1])
+            print(f'Mix Language \033[31m出现错误:\033[0m {e} -> ' + mod_id.split('/')[-1])
             return
         # mix two lang files in to mix.json
         en_file.update(zh_file)
@@ -238,7 +264,7 @@ def initialization_params(args):
     # Default
     output_dir = './output'
     lang_target = 'zh_cn'
-    max_process = 2
+    max_process = 1
     # read settings
     if os.access('./conf.json', os.R_OK):
         with open('./conf.json', 'r', encoding='utf-8') as F:
@@ -281,11 +307,9 @@ if __name__ == '__main__':
     # setting
     helper = LangTransHelper()
     helper.__int__(outputDir, langTarget)
-
+    # create process
     # start
     while True:
-        # create process
-        pool = ProcessPoolExecutor(max_workers=maxProcess)
         # 提示信息
         print(
             '-' * 45 +
@@ -295,11 +319,11 @@ if __name__ == '__main__':
             '-' * 45, end='')
         # choose select
         pType = input('\r\n\033[1mInput your select:\033[0m  ')
-        if pType == 'q':
-            break
-        if pType.lower() not in ('1', '2', '3', 'q'):
-            print('\033[35m无此选项\033[0m')
+        if pType == '':
+            print('未选择')
             continue
+        if pType.lower() == 'q':
+            break
         # Does mix the language
         if input('\033[1m是否将语言混合(y/n):\033[0m  ') == 'y':
             helper.langMix = True
@@ -310,6 +334,8 @@ if __name__ == '__main__':
         # input output direction
         shutil.rmtree(outputDir + '/', ignore_errors=True)
         # select
+        pool = ProcessPoolExecutor(max_workers=maxProcess)
+        start = time.perf_counter()
         if pType == '1':
             for i in helper.scaner_file('input/mods'):
                 pool.submit(helper.extract_mod_lang, i)
@@ -319,7 +345,11 @@ if __name__ == '__main__':
         elif pType == '3':
             firstList = helper.scaner_file('input/mods')
             secondList = helper.scaner_file('input/resource')
+            # helper.replace_authority_lang(firstList, secondList)
             pool.submit(helper.replace_authority_lang, firstList, secondList)
+        else:
+            print('\033[35m无此选项\033[0m')
+            continue
 
         pool.shutdown(wait=True)
         # select three temp folder remove
@@ -328,12 +358,16 @@ if __name__ == '__main__':
         if not os.access(f'{outputDir}/has_trans/', os.W_OK):
             print('\033[31m程序或许未正常运行,请检查输入输出文件夹是否正常\033[0m')
             continue
+
         count = 0
         # mix and sort
         for i in helper.scaner_file(f'{outputDir}/has_trans/'):
             helper.mix_language_files(i)
             helper.sort_files(i)
             count += 1
-        print(f'\n\033[1;32m处理完成,共\033[0m \033[33m{count}\033[0m \033[1;32m个项目\033[0m')
-    print('程序已退出,窗口将在3秒后关闭')
-    sleep(3)
+        end = time.perf_counter()
+        print(
+            f'\n\033[1;32m处理完成,共 \033[33m{count} \033[1;32m个项目\033[0m \n'
+            f'\033[1;32m共耗时 \033[33m{end - start:.3f} \033[1;32m秒\033[0m')
+    print('程序已退出')
+    os.system('pause')
